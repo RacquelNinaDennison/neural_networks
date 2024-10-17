634d18c057a1878a9f9d2953ddb0956e9ac72fdc
Racquel Nina Dennison <94598421+RacquelNinaDennison@users.noreply.github.com>
2024-10-17 14:24:49 +0200

    Add files via upload

---
+++ visualiser.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+names = []
+average_acc = []
+min_acc = []
+best_acc = []
+average_loss = []
+min_loss = []
+max_loss = []
+fileName = open("ExperimentResults.csv",'r')
+data = fileName.read().split("\n")
+fileName.close()
+
+for line in data[1:-1]: # skip first line
+    content = line.split(',')
+    #print(content)
+    names.append(content[0])
+    average_acc.append(float(content[2]))
+    min_acc.append(float(content[4]))
+    best_acc.append(float(content[3]))
+    average_loss.append(float(content[5]))
+    min_loss.append(float(content[7]))
+    max_loss.append(float(content[6]))
+
+bar_colors = ['C0'] * len(names)
+bar_hatches = [""] * len(names)
+bar_colors[13] = 'crimson'
+bar_hatches[13] = "//"
+
+fig, ax = plt.subplots()
+ax.bar(names,average_acc, color=bar_colors, linewidth=1.25, edgecolor='black', hatch=bar_hatches)
+ax.set_ylabel("Accuracy (%)",fontsize=16)
+ax.set_xlabel("Parameter Set",fontsize=16)
+ax.set_title("Results of randomised hyperparameter testing", fontsize=16)
+ax.set_ylim(0.2,0.9)
+
+plt.xticks(rotation=40,fontsize=14)  # X-tick labels font size
+plt.yticks(fontsize=14)  # Y-tick labels font size
+
+for i in range(len(average_acc)):
+    ax.vlines(x=i, ymin=min_acc[i], ymax=best_acc[i], color='black', linestyles='dashed',linewidth=2)
+
+
+
+plt.show()
